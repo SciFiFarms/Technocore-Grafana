010e70245b1d5ee1c5722b9eeb67a150d0d8400b
Spencer Hachmeister <SpencerHachmeister@gmail.com>
2019-05-17 19:46:22 -0600

    Add initial Dockerfile, go-init, entrypoint.sh, and exitpoint.sh.

---
+++ Dockerfile
@@ -0,0 +1,18 @@
+FROM grafana/grafana:6.1.6
+
+USER root
+# Install envsubst. Needed in entrypoint.sh and comes in the gettext-base package.
+RUN apt-get update && apt-get install -y gettext-base && \
+    apt-get autoremove -y && \
+    rm -rf /var/lib/apt/lists/*
+COPY provisioning /etc/grafana/provisioning
+RUN chown -R grafana:grafana "$GF_PATHS_PROVISIONING" 
+USER grafana
+
+# Set up the CMD as well as the pre and post hooks.
+COPY go-init /bin/go-init
+COPY entrypoint.sh /usr/bin/entrypoint.sh
+COPY exitpoint.sh /usr/bin/exitpoint.sh
+
+ENTRYPOINT ["go-init"]
+CMD ["-pre", "entrypoint.sh", "-main", "/run.sh", "-post", "exitpoint.sh"]
---
+++ entrypoint.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+# https://unix.stackexchange.com/questions/79068/how-to-export-variables-that-are-set-all-at-once
+set -a
+GF_SERVER_ROOT_URL=https://${DOCKER_HOST}/grafana/
+
+TIMESERIES_DB_USERNAME=$(cat /run/secrets/timeseries_db_username)
+TIMESERIES_DB_PASSWORD=$(cat /run/secrets/timeseries_db_password)
+set +a
+# TODO: Rather than do this by hand, should loop over all .template files in the folder.
+cat /etc/grafana/provisioning/datasources/technocore-postgres.yaml.template | envsubst > /etc/grafana/provisioning/datasources/technocore-postgres.yaml
+cat /etc/grafana/provisioning/datasources/technocore-influxdb.yaml.template | envsubst > /etc/grafana/provisioning/datasources/technocore-influxdb.yaml
